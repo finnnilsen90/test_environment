262ad8839c1af8efc47285daecec57cc6d81e735
finnnilsen90 <fnilsen1051@gmail.com>
2018-09-02 14:01:24 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+
+const app = express();
+
+let port = process.env.PORT || 3000;
+
+
+// set our application port
+app.set('port', port);
+
+app.use(morgan('dev'));
+
+app.use(express.static(path.join(__dirname, 'slider/d3')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,'/slider/d3/index.html'));
+});
+
+// route for handling 404 requests(unavailable routes
+app.use(function (req, res, next) {
+    res.status(404).send("Sorry can't find that!")
+});
+
+app.listen(app.get('port'), function() {
+    console.log('listening on ', port)
+});
---
+++ slider/d3/slider.js
@@ -0,0 +1,13 @@
+console.log('test')
+
+// Slider Todo
+
+// Need to click div and move it side to side in a limited fashion.
+
+// var slider = document.querySelector('.slider');
+
+// slider.addEventListener('mouseup', () => {console.log(slider.value)}, false)
+
+
+
+// Need to record position
