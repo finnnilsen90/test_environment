4698aa765f8c1c95c89bbf97b57453fecf78a63f
finnnilsen90 <fnilsen1051@gmail.com>
2018-09-05 22:55:51 -0700

    cash formula

--- slider/d3_tutorial/script.js
+++ slider/d3_tutorial/script.js
@@ -1,25 +1,39 @@
 console.log('test')
 
-var data = [30, 86, 168, 281, 303, 365]
+var index = [.30, .80, 1, 1, .90, .80]
+var cash = 5000;
+var cr = .003;
+var ctr =.015;
+var cpm = 10;
+var avg_ord = 80;
 
-function graph(data_par,value) {
+function revenue(cr,ctr,cpm,avg_ord,cash) {
+    var imp = cash/(cpm/1000);
+    var site = imp*ctr;
+    var conv = site*cr;
+    var revenue = conv*avg_ord;
+    return revenue
+}
+
+console.log(revenue(cr,ctr,cpm,avg_ord,cash));
+
+function graph(data_par,value,cr,cash) {
     this.new_data = [];
-    // if (lst_value<value) {
-    //     var chng = this.lst_value*-.01 || 1;
-    // } else {
-    //     var chng = this.lst_value*.01 || 1;
-    // }
     var chng = value*.01 || 1;
-    console.log('slider value => '+chng)
     if (chng!==1) {
-        for (var i=0;i<data.length;i++) {
-            this.new_data.push(Math.round((data_par[i]*chng)+data_par[i]));
+        for (var i=0;i<data_par.length;i++) {
+            var ind_chng = data_par[i];
+            var cr_f = (ind_chng*cr)+ind_chng
+            var new_cash = (cash*value)/+value
+            var rev = revenue(cr,ctr,cpm,avg_ord,new_cash);
+            console.log(cr_f)
+            console.log(rev)
+            this.new_data.push(Math.round(rev));
         }
     } else {
         this.new_data = data_par;
     }
 
-    console.log(this.new_data)
     d3.select('.chart')
         .selectAll('div')
         .data(this.new_data)
@@ -29,7 +43,7 @@ function graph(data_par,value) {
         .text(function(d) { return d; });
 };
 
-graph(data)
+graph(index)
 
 var slider = document.querySelector('.slider');
 slider.addEventListener('mouseup', (c) => {
@@ -40,5 +54,5 @@ slider.addEventListener('mouseup', (c) => {
     var node = document.createElement('div')
     node.className = 'chart'
     parent.appendChild(node)
-    graph(data,slider_val)
+    graph(index,slider_val,cr,cash)
  });
